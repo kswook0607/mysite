d6be5aef0062c9ad64160628cfcc0f7fa4eccc57
seungwook <seungwook@localhost>
2025-09-18 14:29:50 +0900

    ccccc

---
+++ mysite02/src/main/java/com/bit2025/mysite/controller/MainServlet.java
@@ -0,0 +1,28 @@
+package com.bit2025.mysite.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+		String config = getServletConfig().getInitParameter("config");
+		System.out.println("MainServlet.init() called:" + config);
+		super.init();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/main/index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+
+}
